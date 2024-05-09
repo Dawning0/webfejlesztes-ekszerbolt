cd08b7fccf4297790f67057bfb7686bc0c4922b8
Dawning0 <kisscsongor001@gmail.com>
2024-05-09 09:00:57 +0200

    AuthGuard changed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,7 @@ const routes: Routes = [
   {path: 'not-found', loadChildren: () => import('./Pages/not-found/not-found.module').then(m => m.NotFoundModule)},
   {path: 'login', loadChildren: () => import('./Pages/login/login.module').then(m => m.LoginModule)},
   {path: 'register', loadChildren: () => import('./Pages/register/register.module').then(m => m.RegisterModule)},
-  {path: 'cart', loadChildren: () => import('./Pages/cart/cart.module').then(m => m.CartModule) },
+  {path: 'cart', loadChildren: () => import('./Pages/cart/cart.module').then(m => m.CartModule), canActivate : [AuthGuard] },
   {path: '', redirectTo: 'main', pathMatch: 'full'},
   {path: '**', redirectTo: 'not-found'}
 ];
